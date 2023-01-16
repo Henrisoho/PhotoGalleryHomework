edfc3ca0117058748314a55ab0538e5d1029b5bf
henrisoho <bakingwithmymother@gmail.com>
2023-01-16 16:01:56 -0600

    changed if else

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -2,7 +2,7 @@ const galleryItems = [
     { id: 1, path: 'images/g', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
 
     { 
-        id: 1, 
+        id: 2, 
         path: '/Users/hank/Documents/Tier2/week5t2/PhotoGalleryHomework/public/images/istockphoto-184276818-1024x1024.jpg', 
         description: 'Photo of a goat taken at Glacier National Park.', 
         likes: 0 
